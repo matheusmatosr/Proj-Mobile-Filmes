38baa16b001059ca2827d0e388286281ce3fd4dd
Leonarda <leonardaamaral@ucl.br>
2024-06-20 17:56:44 -0300

    atualização da pagina de detalhes

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -32,7 +32,7 @@ class DetailsPage extends StatelessWidget {
   Widget _buildPoster(BuildContext context) {
     String posterPath = item['poster_path'];
     return AspectRatio(
-      aspectRatio: 2 / 3,
+      aspectRatio: 2 / 2.5, // Reduzindo o aspectRatio para diminuir o tamanho
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10.0),
         child: Image.network(
@@ -94,6 +94,22 @@ class DetailsPage extends StatelessWidget {
                           color: Colors.white,
                         ),
                       ),
+                      SizedBox(width: 10),
+                      Text(
+                        _getAgeRestriction(),
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        _getDuration(),
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
                     ],
                   ),
                 ],
@@ -127,5 +143,36 @@ class DetailsPage extends StatelessWidget {
   String _getRating() {
     double rating = item['vote_average'] ?? 0.0;
     return rating.toStringAsFixed(1);
+  }
+
+  String _getAgeRestriction() {
+    String certification = 'Não especificado'; // Inicialmente assume como não especificado
+    List<dynamic>? releaseDates = item['release_dates']?['results']; // Obtém as datas de lançamento
+
+    // Busca pela certificação do país "US" (Estados Unidos) se disponível
+    if (releaseDates != null) {
+      for (var result in releaseDates) {
+        if (result['iso_3166_1'] == 'US') {
+          var certifications = result['release_dates'];
+          for (var cert in certifications) {
+            if (cert['type'] == 3) {
+              certification = cert['certification'] ?? 'Não especificado';
+              break;
+            }
+          }
+          break;
+        }
+      }
+    }
+
+    return '($certification)';
+  }
+
+  String _getDuration() {
+    int runtime = item['runtime'] ?? item['episode_run_time']?.first ?? 0;
+    int hours = runtime ~/ 60;
+    int minutes = runtime % 60;
+    String duration = hours > 0 ? '$hours h $minutes min' : '$minutes min';
+    return duration;
   }
 }
